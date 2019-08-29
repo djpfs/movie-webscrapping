ad2017818cde4403ca9f71f0f04bccf754f255e9
djpfs <43576725+djpfs@users.noreply.github.com>
2019-08-29 17:46:49 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,41 +1,59 @@
 import requests
 import urllib.request
 import time
-from bs4 import BeautifulSoup	
+import json
+from bs4 import BeautifulSoup
+
+TITLE = 'Titulo Original: </b>'
+MOVIE_URI = "https://filmesnomega.info/page/"
+API_KEY = "PlzBanMe"
+OMDB_URI = "http://www.omdbapi.com/?t="
 
 
-def _getMoviesByPage(page):
-	links = []
-	response = requests.get("https://filmesnomega.info/page/" + str(page))
-	soup = BeautifulSoup(response.text, "html.parser")
-	posts = soup.findAll("a", {"class": "readmore"})
+def getMoviesByPage(page):
+    links = []
+    response = requests.get(MOVIE_URI + str(page))
+    soup = BeautifulSoup(response.text, "html.parser")
+    posts = soup.findAll("a", {"class": "readmore"})
 
-	for post in posts:
-		links.append(post['href'])
+    for post in posts:
+        links.append(post['href'])
 
-	return links
+    return links
 
-def _getMovieUrl(link):
-	response = requests.get(link)
-	soup = BeautifulSoup(response.text, "html.parser")
-	detail = soup.findAll("div", {"class": "separator"})
-	
-	try:
-		link = str(detail[2]).split('href="')
-		name = str(soup.findAll("span")[2]).split('Titulo Original: </b>')[1].replace("</span>", "")
-		return {"url": link[1].split('"')[0], "name": name}
-	except:
-		return "" 
-		  	
+
+def getMovieUrl(link):
+    response = requests.get(link)
+    soup = BeautifulSoup(response.text, "html.parser")
+    detail = soup.findAll("div", {"class": "separator"})
+
+    try:
+        link = str(detail[2]).split('href="')
+        name = str(soup.findAll("span")[2]).split(TITLE)[1].replace('</span>', '')
+        return {"url": link[1].split('"')[0], "name": name, "imdb": _imdbSearch(name)}
+    except:
+        return ""
+
+
+def _imdbSearch(movieName):
+    response = requests.get(OMDB_URI + movieName + "&apikey=" + API_KEY)
+    imdbResult = json.loads(response.text)
+    return imdbResult['imdbID']
+
+
+
 downloads = []
- 	
-for page in range(1, 10):
-	links = _getMoviesByPage(page)
 
-	for movie in links:
-		url = _getMovieUrl(movie)
-		if (url != ""):
-			downloads.append(url)
+for page in range(1, 10):
+    links = getMoviesByPage(page)
+
+    for movie in links:
+        url = getMovieUrl(movie)
+        if url != "":
+            downloads.append(url)
 
 for downloadMovieUrl in downloads:
-		print(downloadMovieUrl)
+    print(downloadMovieUrl)
+
+
+
